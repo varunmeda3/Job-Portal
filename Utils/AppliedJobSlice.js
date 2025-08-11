3f3f4e4af5e52ebcfcfc8c596988003e3658ad99
varunmeda3 <medavarun3@gmail.com>
2025-08-11 15:31:03 -0400

    Update AppliedJobSlice

--- Utils/AppliedJobSlice.js
+++ Utils/AppliedJobSlice.js
@@ -1,1 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit'
 
+const initialState = {
+   appliedJob : [],
+   bookMark : [],
+}
+
+export const appliedJobSlice = createSlice({
+  name: 'AppliedJob',
+  initialState,
+  reducers: {
+    setAppliedJob : (state, action) => {
+        state.appliedJob = action.payload
+    },
+    setBookMark : (state , action) => {
+      state.bookMark = action.payload
+    }
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { setAppliedJob  , setBookMark} = appliedJobSlice.actions
+
+export const AppliedJobReducer = appliedJobSlice.reducer
+
+// 
